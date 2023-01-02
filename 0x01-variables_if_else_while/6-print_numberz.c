f839e83b9f7e5899ea1f04476aa058cbed400c4e
William <ikanyeng89@gmail.com>
2023-01-02 07:31:52 -0800

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio>
+
+/**
+ * main -  Entry point
+ * Return: Always return 0 (success)
+ */
+
+inr main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		putchar(c + 'a');
+	putchar('\n');
+
+	return (0);
+}
